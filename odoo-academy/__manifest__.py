4810e6dea6b75391ca8b28cc183a5fcfeb00b4e8
MarielSarai <marielsaraigar@gmail.com>
2021-09-17 22:55:47 +0000

    Se agregó un nuevo Modulo

---
+++ odoo-academy/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoo Academy',
+    'summary': """Academy app to manage Training""",
+    'description': """
+    Academy Module to manage Training:
+        - Courses
+        - Sessions
+        - Attendees
+    """,
+    'autor': 'Odoo',x|
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': {
+        
+    }
+    'demo':[
+  ],
+}
